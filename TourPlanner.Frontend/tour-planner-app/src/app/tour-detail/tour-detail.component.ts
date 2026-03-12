93fde8fb91c6fea2ab24214f756e0ef8b7d02d1a
Stefan Vukmirovic <if24b267@technikum-wien.at>
2026-03-12 14:33:17 +0100

    Fix frontend structure and regenerate components

---
+++ TourPlanner.Frontend/tour-planner-app/src/app/tour-detail/tour-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-detail',
+  imports: [],
+  templateUrl: './tour-detail.component.html',
+  styleUrl: './tour-detail.component.css'
+})
+export class TourDetailComponent {
+
+}
